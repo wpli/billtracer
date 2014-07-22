d64e2419d88f29ee6d041c2ac363120ca08ee515
sophie <sbchou@gmail.com>
2014-07-22 14:08:05 +0000

    add exception catching

--- states/code/parsers/ca.py
+++ states/code/parsers/ca.py
@@ -6,24 +6,37 @@ from URLS
 import re
 import requests
 from bs4 import BeautifulSoup as BS 
+from requests.exceptions import ConnectionError
 
 r = requests.get('http://projects.csail.mit.edu/legalgenome/states_smalldata/ca.bill_urls.txt')
 urls = r.text.split()
 total = len(urls)
 
-i = 1
+i = 0
 for url in urls:
+    i += 1
+
     bill_id = re.findall('.*bill_id=(.*)', url)[0]
     print "SCRAPING", bill_id, str(i), "/", total
-    page = requests.get(url)
-    soup = BS(page.text)
-    all_texts = soup.findAll(text=True)
-    body = soup.findAll('div', {'id': 'bill_all'})
-    texts = [t.text.strip() for t in body]
-    encoded = [t.encode('utf8') for t in texts]        
+    try:
+        page = requests.get(url)
+        soup = BS(page.text)
+        all_texts = soup.findAll(text=True)
+        body = soup.findAll('div', {'id': 'bill_all'})
+   
+        texts = [t.text.strip() for t in body]
+        encoded = [t.encode('utf8') for t in texts]        
+        
+        out_file = open('../../data/ca/' + bill_id + '.txt', 'w')
+        for t in encoded:
+            out_file.write(t)
+        out_file.close()
     
-    out_file = open('../../data/ca/' + bill_id + '.txt', 'w')
-    for t in encoded:
-        out_file.write(t)
-    out_file.close()
-    i += 1
+    except ConnectionError:
+        print "can't connect"
+        continue
+
+    except AttributeError:
+        print "can't get text"
+        continue
+           
